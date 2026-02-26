d22e7da99ce08b68fd3a5135cd2a01cc993b6685
Kai Joshua Martin <kaijoshua.martin@perfact.de>
2026-02-26 14:17:12 +0100

    T294880: Add allowedclasses module (#15)
    Add allowedclasses module that will be considered when https://github.com/perfact/ZPerFactMods/pull/21 is merged
    
    Reviewed-on: https://git.perfact.de/DebianPackages/python-perfact-dbutils/pulls/15
    Reviewed-by: Viktor Dick <vd@perfact.de>
    Co-authored-by: Kai Joshua Martin <kaijoshua.martin@perfact.de>
    Co-committed-by: Kai Joshua Martin <kaijoshua.martin@perfact.de>

---
+++ perfact/dbutils/allowedclasses.py
@@ -0,0 +1,11 @@
+from .api import User, LCC, Context
+from .conn import ZRDBConnectionWrapper, Namespace, Results
+
+__all__ = [
+    "User",
+    "LCC",
+    "Context",
+    "ZRDBConnectionWrapper",
+    "Namespace",
+    "Results",
+]
